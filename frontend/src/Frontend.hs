898b35b48d0436e1020488c7fc8556310035b7fa
Adriaan Leijnse <parenthetical@users.noreply.github.com>
2022-09-21 20:41:04 +0100

    Connect to the first node from front-end and send init

--- frontend/src/Frontend.hs
+++ frontend/src/Frontend.hs
@@ -1,5 +1,10 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE RecursiveDo #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE PartialTypeSignatures #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Frontend where
 
@@ -14,9 +19,11 @@ import Obelisk.Route
 import Obelisk.Generated.Static
 
 import Reflex.Dom.Core
-
+import Reflex.Dom.WebSocket
+import Reflex.Dom.Prerender
 import Common.Api
 import Common.Route
+import Control.Monad.IO.Class (liftIO)
 
 
 -- This runs in a monad that can be run on the client or the server.
@@ -27,7 +34,16 @@ frontend = Frontend
   { _frontend_head = do
       el "title" $ text "Obelisk Minimal Example"
       elAttr "link" ("href" =: $(static "main.css") <> "type" =: "text/css" <> "rel" =: "stylesheet") blank
-  , _frontend_body = do
-
-      return ()
+  , _frontend_body = prerender_ (pure ()) frontend'
   }
+
+frontend' :: forall t m. (DomBuilder t m,
+                   PostBuild t m, TriggerEvent t m, PerformEvent t m, MonadHold t m, _) => m ()
+frontend' = mdo
+  text "Hello"
+  let wsCfg :: (WebSocketConfig t T.Text) = WebSocketConfig toSend never False []
+  ws <- textWebSocket "ws://localhost:9001" wsCfg
+  performEvent_ (liftIO . putStrLn . T.unpack <$> _webSocket_recv ws)
+  performEvent_ (liftIO . print <$> toSend)
+  let toSend = ["{ \"tag\": \"Init\", \"contestationPeriod\": 60 }" ] <$ _webSocket_open ws
+  return ()
