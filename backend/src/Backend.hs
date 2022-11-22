26c97defbae7fdeb721bffac8615b78411758fc5
Adriaan Leijnse <parenthetical@users.noreply.github.com>
2022-11-22 11:11:52 +0000

    Make project compile + cleanup

--- backend/src/Backend.hs
+++ backend/src/Backend.hs
@@ -15,54 +15,36 @@ import Prelude hiding (filter)
 import Hydra.Types
 import Hydra.Devnet
 
-import Data.Maybe
-import Data.Map (Map)
-import qualified Data.Map as Map
 
-import Common.Helpers
 import Common.Route
 import Obelisk.Backend
 import Obelisk.Route
-import Snap.Core
 
 import Control.Monad.Log
-import Control.Monad.IO.Class (MonadIO, liftIO)
+import Control.Monad.IO.Class (liftIO)
 
-import Data.Foldable
 
-import Data.String.Interpolate ( i )
-import qualified Data.Text as T
-import qualified Data.Text.IO as T
-import Data.Text.Prettyprint.Doc
 
-import qualified System.IO.Streams as Streams
 
 import Control.Concurrent
-import System.Process
 
 import Data.Aeson as Aeson
-    ( decode, encode, FromJSON, ToJSON )
+    ( decode, encode )
 
-import qualified Data.ByteString.Lazy as LBS
 
 import HydraPay
-import HydraPay.Client
 
-import CardanoNodeInfo
 
 import HydraPay.Api
-import Control.Monad ((<=<), forever, when, guard)
+import Control.Monad (forever)
 
 import Network.WebSockets.Snap
 import qualified Network.WebSockets as WS
 import Data.Traversable (forM)
-import Text.Read (readMaybe)
-import Data.Maybe (fromMaybe)
-import Control.Monad.Trans.Maybe
-import System.Directory (doesFileExist)
 import qualified HydraPay.WebSocketDemo as WSD
+import Data.Text.Prettyprint.Doc (Doc)
 
-setupDemo :: State -> IO ([(KeyPair, Address)])
+setupDemo :: State -> IO [(KeyPair, Address)]
 setupDemo state = do
   cns <- readMVar (_state_cardanoNodeState state)
   case cardanoNodeState_nodeType cns of
@@ -83,13 +65,14 @@ setupDemo state = do
         addr <- liftIO $ getCardanoAddress (_previewNodeInfo pcfg) (_verificationKey kp)
         pure (kp, addr)
 
+runLogging :: LoggingT (WithSeverity (Doc ann)) IO a -> IO a
 runLogging = flip runLoggingT (print . renderWithSeverity id)
 
 backend :: Backend BackendRoute FrontendRoute
 backend = Backend
   { _backend_run = \serve -> do
       -- NOTE(skylar): Running heads is a map from head name to network handle
-      flip runLoggingT (print . renderWithSeverity id) $ do
+      runLogging $ do
         liftIO $ runHydraPay $ \state -> do
           hsi <- getHydraSharedInfo state
           participants <- setupDemo state
--- backend/src/HydraPay/SubmitTxDirectTest.hs
+++ backend/src/HydraPay/SubmitTxDirectTest.hs
@@ -1,196 +1,196 @@
 module HydraPay.SubmitTxDirectTest where
 
-import HydraPay
-import CardanoNodeInfo
-import Hydra.Devnet
-import System.Process
-import Control.Monad.Log
-import Control.Monad.IO.Class (MonadIO)
+-- import HydraPay
+-- import CardanoNodeInfo
+-- import Hydra.Devnet
+-- import System.Process
+-- import Control.Monad.Log
+-- import Control.Monad.IO.Class (MonadIO)
 
-import qualified Data.Text as T
-import Data.Text.Prettyprint.Doc
+-- import qualified Data.Text as T
+-- import Data.Text.Prettyprint.Doc
 
-import Control.Concurrent
-import HydraPay.Client
-import Control.Exception (finally)
+-- import Control.Concurrent
+-- import HydraPay.Client
+-- import Control.Exception (finally)
 
-import HydraPay.Api
-import Hydra.Types
-import Data.Either
-import qualified Data.ByteString.Lazy as LBS
-import qualified Data.Aeson as Aeson
-import System.IO
-import System.IO.Temp
+-- import HydraPay.Api
+-- import Hydra.Types
+-- import Data.Either
+-- import qualified Data.ByteString.Lazy as LBS
+-- import qualified Data.Aeson as Aeson
+-- import System.IO
+-- import System.IO.Temp
 
-runLog :: LoggingT (WithSeverity (Doc ann)) IO a -> IO a
-runLog = flip runLoggingT (print . renderWithSeverity id)
+-- runLog :: LoggingT (WithSeverity (Doc ann)) IO a -> IO a
+-- runLog = flip runLoggingT (print . renderWithSeverity id)
 
-getDevnetHydraSharedInfo :: (MonadIO m, MonadLog (WithSeverity (Doc ann)) m) => m HydraSharedInfo
-getDevnetHydraSharedInfo = do
-  scripts <- getReferenceScripts "devnet/scripts" (_signingKey devnetFaucetKeys)
-  pure $ HydraSharedInfo
-    { _hydraScriptsTxId = T.unpack scripts,
-        _cardanoNodeInfo = cardanoDevnetNodeInfo
-      }
+-- getDevnetHydraSharedInfo :: (MonadIO m, MonadLog (WithSeverity (Doc ann)) m) => m HydraSharedInfo
+-- getDevnetHydraSharedInfo = do
+--   scripts <- getReferenceScripts "devnet/scripts" (_signingKey devnetFaucetKeys)
+--   pure $ HydraSharedInfo
+--     { _hydraScriptsTxId = T.unpack scripts,
+--         _cardanoNodeInfo = cardanoDevnetNodeInfo
+--       }
 
-previewScriptsId = "4081fab39728fa3c05c0edc4dc7c0e8c45129ca6b2b70bf8600c1203a79d2c6d"
+-- previewScriptsId = "4081fab39728fa3c05c0edc4dc7c0e8c45129ca6b2b70bf8600c1203a79d2c6d"
 
-previewNodeInfo =
-  CardanoNodeInfo
-    { _nodeType = TestNet 2,
-      _nodeSocket = "/tmp/cardano-node.socket",
-      _nodeLedgerGenesis = "/etc/nixos/cardano-preview-testnet-config/shelley-genesis.json",
-      _nodeLedgerProtocolParameters = "preview-parameters.json"
-    }
+-- previewNodeInfo =
+--   CardanoNodeInfo
+--     { _nodeType = TestNet 2,
+--       _nodeSocket = "/tmp/cardano-node.socket",
+--       _nodeLedgerGenesis = "/etc/nixos/cardano-preview-testnet-config/shelley-genesis.json",
+--       _nodeLedgerProtocolParameters = "preview-parameters.json"
+--     }
 
-previewHydraSharedInfo =
-  HydraSharedInfo 
-  { _hydraScriptsTxId = previewScriptsId,
-    _cardanoNodeInfo = previewNodeInfo
-  }
-
-
-signAndSubmitTx' cninf (KeyPair sk _) tx = do
-  withTempFile "." "tx.draft" $ \draftFile draftHandle -> do
-    LBS.hPut draftHandle $ Aeson.encode tx
-    hClose draftHandle
-
-    withTempFile "." "tx.signed" $ \signedFile signedHandle -> do
-      hClose signedHandle
-      let cp = (proc cardanoCliPath [ "transaction"
-                                    , "sign"
-                                    , "--tx-body-file"
-                                    , draftFile
-                                    , "--signing-key-file"
-                                    , getSigningKeyFilePath sk
-                                    , "--out-file"
-                                    , signedFile
-                                    ])
-            { env = Just [( "CARDANO_NODE_SOCKET_PATH" , _nodeSocket cninf)]
-            }
-      _ <- readCreateProcess cp ""
-      submitTx cninf signedFile >>= withLogging . waitForTxIn cninf
+-- previewHydraSharedInfo =
+--   HydraSharedInfo 
+--   { _hydraScriptsTxId = previewScriptsId,
+--     _cardanoNodeInfo = previewNodeInfo
+--   }
 
 
-runWDevnet :: (CardanoNodeInfo -> HydraSharedInfo -> KeyPair -> IO a) -> IO a
-runWDevnet f = do 
-  _ <- readCreateProcess (shell "rm -rf devnet") ""
-  _ <- readCreateProcess (shell "rm -rf demo-logs") ""
-  runLog $ prepareDevnet
-  createProcess cardanoNodeCreateProcess
-  threadDelay $ seconds 3
-  shinfo <- runLog getDevnetHydraSharedInfo
-  f cardanoDevnetNodeInfo shinfo devnetFaucetKeys
+-- signAndSubmitTx' cninf (KeyPair sk _) tx = do
+--   withTempFile "." "tx.draft" $ \draftFile draftHandle -> do
+--     LBS.hPut draftHandle $ Aeson.encode tx
+--     hClose draftHandle
 
-runWPreview :: (CardanoNodeInfo -> HydraSharedInfo -> KeyPair -> IO a) -> IO a
-runWPreview f = do 
-  f previewNodeInfo previewHydraSharedInfo (mkKeyPair "test-keys/a.cardano.sk" "test-keys/a.cardano.vk")
+--     withTempFile "." "tx.signed" $ \signedFile signedHandle -> do
+--       hClose signedHandle
+--       let cp = (proc cardanoCliPath [ "transaction"
+--                                     , "sign"
+--                                     , "--tx-body-file"
+--                                     , draftFile
+--                                     , "--signing-key-file"
+--                                     , getSigningKeyFilePath sk
+--                                     , "--out-file"
+--                                     , signedFile
+--                                     ])
+--             { env = Just [( "CARDANO_NODE_SOCKET_PATH" , _nodeSocket cninf)]
+--             }
+--       _ <- readCreateProcess cp ""
+--       submitTx cninf signedFile >>= withLogging . waitForTxIn cninf
+
+
+-- runWDevnet :: (CardanoNodeInfo -> HydraSharedInfo -> KeyPair -> IO a) -> IO a
+-- runWDevnet f = do 
+--   _ <- readCreateProcess (shell "rm -rf devnet") ""
+--   _ <- readCreateProcess (shell "rm -rf demo-logs") ""
+--   runLog $ prepareDevnet
+--   createProcess cardanoNodeCreateProcess
+--   threadDelay $ seconds 3
+--   shinfo <- runLog getDevnetHydraSharedInfo
+--   f cardanoDevnetNodeInfo shinfo devnetFaucetKeys
+
+-- runWPreview :: (CardanoNodeInfo -> HydraSharedInfo -> KeyPair -> IO a) -> IO a
+-- runWPreview f = do 
+--   f previewNodeInfo previewHydraSharedInfo (mkKeyPair "test-keys/a.cardano.sk" "test-keys/a.cardano.vk")
 
 
 
-testTransferAll cninf hydraSharedInfo faucetKeys = do
-  oneKs <- runLog $ generateCardanoKeys "tmp/one"
-  print oneKs
-  one <- getCardanoAddress cninf $ _verificationKey oneKs
-  twoKs <- runLog $ generateCardanoKeys "tmp/two"
-  print twoKs
-  two <- getCardanoAddress cninf $ _verificationKey twoKs
-  runLog $ seedAddressFromFaucetAndWait cninf faucetKeys one (ada 100) False
+-- testTransferAll cninf hydraSharedInfo faucetKeys = do
+--   oneKs <- runLog $ generateCardanoKeys "tmp/one"
+--   print oneKs
+--   one <- getCardanoAddress cninf $ _verificationKey oneKs
+--   twoKs <- runLog $ generateCardanoKeys "tmp/two"
+--   print twoKs
+--   two <- getCardanoAddress cninf $ _verificationKey twoKs
+--   runLog $ seedAddressFromFaucetAndWait cninf faucetKeys one (ada 100) False
 
-  print =<< queryAddressUTXOs cninf one
-  print =<< queryAddressUTXOs cninf two
-  Just txin <- transferAll cninf (_signingKey oneKs) one two
-  print txin
-  runLog $ waitForTxIn cninf txin
-  print =<< queryAddressUTXOs cninf one
-  print =<< queryAddressUTXOs cninf two
-  pure ()
+--   print =<< queryAddressUTXOs cninf one
+--   print =<< queryAddressUTXOs cninf two
+--   Just txin <- transferAll cninf (_signingKey oneKs) one two
+--   print txin
+--   runLog $ waitForTxIn cninf txin
+--   print =<< queryAddressUTXOs cninf one
+--   print =<< queryAddressUTXOs cninf two
+--   pure ()
 
--- (state, ((one,oneKs), (two,twoKs))) <- runWDevnet testFn
-testFn :: CardanoNodeInfo -> HydraSharedInfo -> KeyPair -> IO (State, ((Address, KeyPair), (Address, KeyPair)))
-testFn cninf hydraSharedInfo faucetKeys = do
-  oneKs <- runLog $ generateCardanoKeys "tmp/one"
-  print oneKs
-  one <- getCardanoAddress cninf $ _verificationKey oneKs
-  twoKs <- runLog $ generateCardanoKeys "tmp/two"
-  print twoKs
-  two <- getCardanoAddress cninf $ _verificationKey twoKs
+-- -- (state, ((one,oneKs), (two,twoKs))) <- runWDevnet testFn
+-- testFn :: CardanoNodeInfo -> HydraSharedInfo -> KeyPair -> IO (State, ((Address, KeyPair), (Address, KeyPair)))
+-- testFn cninf hydraSharedInfo faucetKeys = do
+--   oneKs <- runLog $ generateCardanoKeys "tmp/one"
+--   print oneKs
+--   one <- getCardanoAddress cninf $ _verificationKey oneKs
+--   twoKs <- runLog $ generateCardanoKeys "tmp/two"
+--   print twoKs
+--   two <- getCardanoAddress cninf $ _verificationKey twoKs
 
-  let nodz = [one,two]
+--   let nodz = [one,two]
 
-  state <- getHydraPayState hydraSharedInfo
+--   state <- getHydraPayState hydraSharedInfo
 
-  putStrLn "Seeding..."
-  runLog $ seedAddressFromFaucetAndWait cninf faucetKeys one (ada 200) False
-  runLog $ seedAddressFromFaucetAndWait cninf faucetKeys two (ada 200) False
+--   putStrLn "Seeding..."
+--   runLog $ seedAddressFromFaucetAndWait cninf faucetKeys one (ada 200) False
+--   runLog $ seedAddressFromFaucetAndWait cninf faucetKeys two (ada 200) False
 
-  flip finally (do
-                   putStrLn "CLEANING UP"
-                   terminateHead state "a"
-               ) $ do
-    putStrLn "Creating head"
-    _ <- createHead state (HeadCreate "a" nodz)
+--   flip finally (do
+--                    putStrLn "CLEANING UP"
+--                    terminateHead state "a"
+--                ) $ do
+--     putStrLn "Creating head"
+--     _ <- createHead state (HeadCreate "a" nodz)
 
-    putStrLn "Doing setup txs"
-    let doTx typ addr keys adaAmount= do
-          print "buildAddTx"
-          (Right x) :: Either HydraPayError Tx <- buildAddTx typ state addr (ada adaAmount)
-          print "Signing"
-          signAndSubmitTx' cninf keys $ x
-    doTx Funds one oneKs 70
-    doTx Funds two twoKs 50
-    doTx Fuel one oneKs 100
-    doTx Fuel two twoKs 100
+--     putStrLn "Doing setup txs"
+--     let doTx typ addr keys adaAmount= do
+--           print "buildAddTx"
+--           (Right x) :: Either HydraPayError Tx <- buildAddTx typ state addr (ada adaAmount)
+--           print "Signing"
+--           signAndSubmitTx' cninf keys $ x
+--     doTx Funds one oneKs 70
+--     doTx Funds two twoKs 50
+--     doTx Fuel one oneKs 100
+--     doTx Fuel two twoKs 100
 
-    putStrLn "Initing head"
-    initHead state (HeadInit "a" one 5)
+--     putStrLn "Initing head"
+--     initHead state (HeadInit "a" one 5)
     
-    threadDelay $ seconds 5
+--     threadDelay $ seconds 5
 
-    putStrLn "Committing to nodes"
-    threadDelay $ seconds 5
-    commitToHead state $ HeadCommit "a" one (ada 70)
-    threadDelay $ seconds 5
-    commitToHead state $ HeadCommit "a" two (ada 50)
+--     putStrLn "Committing to nodes"
+--     threadDelay $ seconds 5
+--     commitToHead state $ HeadCommit "a" one (ada 70)
+--     threadDelay $ seconds 5
+--     commitToHead state $ HeadCommit "a" two (ada 50)
     
-    -- threadDelay $ seconds 3
-    -- -- putStrLn "NodeUTXOS one"
-    -- -- print =<< withNode state "a" one (\x y -> fmap Right . getNodeUtxos x $ y)
-    -- -- threadDelay $ seconds 3
-    -- -- print =<< withNode state "a" two (\x y -> fmap Right . getNodeUtxos x $ y)
-    -- putStrLn ""
-    -- threadDelay $ seconds 10
-    -- -- submitTxOnHead state one (HeadSubmitTx "a" two (ada 20))
-    -- -- threadDelay $ seconds 1
-    -- -- putStrLn "NodeUTXOS one"
-    -- -- oneFunds <- headBalance state "a" one
-    -- -- print oneFunds
-    -- -- threadDelay $ seconds 1
-    -- -- putStrLn "NodeUTXOS two"
-    -- -- twoFunds <- headBalance state "a" two
-    -- -- print twoFunds
+--     -- threadDelay $ seconds 3
+--     -- -- putStrLn "NodeUTXOS one"
+--     -- -- print =<< withNode state "a" one (\x y -> fmap Right . getNodeUtxos x $ y)
+--     -- -- threadDelay $ seconds 3
+--     -- -- print =<< withNode state "a" two (\x y -> fmap Right . getNodeUtxos x $ y)
+--     -- putStrLn ""
+--     -- threadDelay $ seconds 10
+--     -- -- submitTxOnHead state one (HeadSubmitTx "a" two (ada 20))
+--     -- -- threadDelay $ seconds 1
+--     -- -- putStrLn "NodeUTXOS one"
+--     -- -- oneFunds <- headBalance state "a" one
+--     -- -- print oneFunds
+--     -- -- threadDelay $ seconds 1
+--     -- -- putStrLn "NodeUTXOS two"
+--     -- -- twoFunds <- headBalance state "a" two
+--     -- -- print twoFunds
 
-    waitForHeadStatus state "a" Status_Open
-    threadDelay $ seconds 2
-    closeHead state "a"
-    waitForHeadStatus state "a" Status_Finalized 
+--     waitForHeadStatus state "a" Status_Open
+--     threadDelay $ seconds 2
+--     closeHead state "a"
+--     waitForHeadStatus state "a" Status_Finalized 
 
-    threadDelay $ seconds 2
+--     threadDelay $ seconds 2
 
-    -- print "Trying withdraw"
-    -- -- TODO: How do I do this without risking InsufficientFunds?
-    oneFnds <- getProxyFunds state one
-    print oneFnds
-    print =<< withdraw state (WithdrawRequest one Nothing)
-    twoFnds <- getProxyFunds state two
-    print twoFnds
-    print =<< withdraw state (WithdrawRequest two Nothing)
-    -- print =<< withdraw state (WithdrawRequest two (ada ))
+--     -- print "Trying withdraw"
+--     -- -- TODO: How do I do this without risking InsufficientFunds?
+--     oneFnds <- getProxyFunds state one
+--     print oneFnds
+--     print =<< withdraw state (WithdrawRequest one Nothing)
+--     twoFnds <- getProxyFunds state two
+--     print twoFnds
+--     print =<< withdraw state (WithdrawRequest two Nothing)
+--     -- print =<< withdraw state (WithdrawRequest two (ada ))
 
-    -- print ""
-    -- print =<< getProxyFunds state one
-    -- print ""
-    -- print =<< getProxyFunds state two
+--     -- print ""
+--     -- print =<< getProxyFunds state one
+--     -- print ""
+--     -- print =<< getProxyFunds state two
 
-    pure ()
-  pure (state, ((one, oneKs), (two, twoKs)))
+--     pure ()
+--   pure (state, ((one, oneKs), (two, twoKs)))
