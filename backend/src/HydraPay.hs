eba938566fa5310d8a580e9c6a963afd2aa0f072
Skylar-Ray O'Quinn <soraskymizu@gmail.com>
2022-12-01 00:51:02 +0000

    Remove un-needed pragmas

--- backend/src/HydraPay.hs
+++ backend/src/HydraPay.hs
@@ -1,14 +1,4 @@
 -- | 
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE DeriveAnyClass #-}
-{-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE TupleSections #-}
-{-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE PartialTypeSignatures #-}
-{-# OPTIONS_GHC -Wno-unused-matches #-}
-
 module HydraPay (module Export) where
 
 import HydraPay.Server as Export
