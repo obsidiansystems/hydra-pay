cf5b674712e114cc2949a8b62329618e23f869e0
Luigy Leon <luigy@outlook.com>
2023-04-06 16:13:31 -0400

    Setup for watching cardano node socket

---
+++ hydra-pay/src/HydraPay/Watch.hs
@@ -0,0 +1,30 @@
+module HydraPay.Watch where
+
+import Control.Concurrent
+import Control.Monad.IO.Class
+import Reflex
+import Reflex.FSNotify
+import qualified System.FSNotify as FS
+
+import HydraPay.Host
+
+wrapWatch
+  :: (MonadIO m, TriggerEvent t m)
+  => (FS.WatchManager -> pathinfo -> FS.Action -> IO a)
+  -> FS.WatchConfig
+  -> pathinfo
+  -> m (Event t FSEvent)
+wrapWatch f cfg path = do
+  (ev, fire) <- newTriggerEvent
+  void $ liftIO $ forkIO $ FS.withManagerConf cfg $ \mgr -> do
+    _ <- f mgr path fire
+    forever $ threadDelay $ 1000 * 1000
+  pure ev
+
+watchDir
+  :: (TriggerEvent t m, MonadIO m)
+  => FS.WatchConfig
+  -> FilePath
+  -> FS.ActionPredicate
+  -> m (Event t FSEvent)
+watchDir cfg path evFilter = wrapWatch' (\mgr p action -> FS.watchDir mgr p evFilter action) cfg path
