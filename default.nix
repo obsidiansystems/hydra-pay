c34a15334817c332cbac73af57b25865a064145c
Sky O <soraskymizu@gmail.com>
2023-04-07 16:48:17 +0000

    Ensure hydra-pay builds in isolation

--- default.nix
+++ default.nix
@@ -1,41 +1,30 @@
 { system ? builtins.currentSystem
-, obelisk ? import ./.obelisk/impl {
-    inherit system;
-    iosSdkVersion = "13.2";
-
-    # You must accept the Android Software Development Kit License Agreement at
-    # https://developer.android.com/studio/terms in order to build Android apps.
-    # Uncomment and set this to `true` to indicate your acceptance:
-    config.android_sdk.accept_license = true;
-
-    # In order to use Let's Encrypt for HTTPS deployments you must accept
-    # their terms of service at https://letsencrypt.org/repository/.
-    # Uncomment and set this to `true` to indicate your acceptance:
-    terms.security.acme.acceptTerms = true;
-
-    useGHC810 = true;
+, android-build ? false
+, cardanoProject ? import ./dep/cardano-project {
+    inherit android-build system;
   }
 }:
+with cardanoProject;
 with obelisk;
 let
   foldExtensions = lib.foldr lib.composeExtensions (_: _: {});
-  deps = obelisk.nixpkgs.thunkSet ./dep;
+  deps = nixpkgs.thunkSet ./dep;
   flake-compat = import deps.flake-compat;
   hydra = (flake-compat {
     inherit system;
     src = deps.hydra;
   }).defaultNix.packages.${system};
   cardano-node = import deps.cardano-node {};
-
   pkgs = obelisk.nixpkgs;
   livedoc-devnet-script = pkgs.runCommand "livedoc-devnet-script" { } ''
     cp -r ${./livedoc-devnet} $out
   '';
-  p = project ./. ({ pkgs, ... }:
+  p = project ./. ({ pkgs, ... }@args:
     let
+      pd = cardanoProjectDef args;
       haskellLib = pkgs.haskell.lib;
     in
-    {
+    pkgs.lib.recursiveUpdate pd {
       android.applicationId = "systems.obsidian.obelisk.examples.minimal";
       android.displayName = "Obelisk Minimal Example";
       ios.bundleIdentifier = "systems.obsidian.obelisk.examples.minimal";
@@ -44,34 +33,44 @@ let
       packages =
       {
         hydra-pay = ./hydra-pay;
-        cardano-transaction = ./hydra-pay/dep/cardano-transaction-builder;
+        cardano-transaction = pkgs.hackGet ./dep/cardano-transaction-builder;
+        bytestring-aeson-orphans = pkgs.hackGet ./dep/bytestring-aeson-orphans;
       };
 
-      overrides = foldExtensions [
-        (self: super: {
-          aeson-gadt-th = haskellLib.disableCabalFlag (self.callCabal2nix "aeson-gadt-th" deps.aeson-gadt-th {}) "build-readme";
-          reflex-gadt-api = self.callCabal2nix "reflex-gadt-api" deps.reflex-gadt-api {};
-          string-interpolate = haskellLib.doJailbreak (haskellLib.dontCheck super.string-interpolate);
+      overrides = self: super: pd.overrides self super // {
+        aeson-gadt-th = haskellLib.disableCabalFlag (self.callCabal2nix "aeson-gadt-th" deps.aeson-gadt-th {}) "build-readme";
+        reflex-gadt-api = self.callCabal2nix "reflex-gadt-api" deps.reflex-gadt-api {};
+        string-interpolate = haskellLib.doJailbreak (haskellLib.dontCheck super.string-interpolate);
 
-          cardano-transaction = haskellLib.overrideCabal super.cardano-transaction (drv: {
-            librarySystemDepends = (drv.librarySystemDepends or []) ++ [
-              cardano-node.cardano-cli
-            ];
-          });
+        bytestring-aeson-orphans = haskellLib.doJailbreak super.bytestring-aeson-orphans;
 
-          backend = haskellLib.overrideCabal super.backend (drv: {
-            librarySystemDepends = (drv.librarySystemDepends or []) ++ [
-              cardano-node.cardano-node
-              cardano-node.cardano-cli
-              hydra.hydra-node.package.components.exes.hydra-node
-              hydra.hydra-node.package.components.exes.hydra-tools
-              pkgs.jq
-              pkgs.coreutils
-              livedoc-devnet-script
-            ];
-          });
-        })
-      ];
+        cardano-transaction = haskellLib.overrideCabal super.cardano-transaction (drv: {
+          librarySystemDepends = (drv.librarySystemDepends or []) ++ [
+            cardano-node.cardano-cli
+          ];
+        });
+
+        hydra-pay = haskellLib.overrideCabal super.hydra-pay (drv: {
+          librarySystemDepends = (drv.librarySystemDepends or []) ++ [
+            cardano-node.cardano-node
+            cardano-node.cardano-cli
+            hydra.hydra-node.package.components.exes.hydra-node
+            hydra.hydra-node.package.components.exes.hydra-tools
+          ];
+        });
+
+        backend = haskellLib.overrideCabal super.backend (drv: {
+          librarySystemDepends = (drv.librarySystemDepends or []) ++ [
+            cardano-node.cardano-node
+            cardano-node.cardano-cli
+            hydra.hydra-node.package.components.exes.hydra-node
+            hydra.hydra-node.package.components.exes.hydra-tools
+            pkgs.jq
+            pkgs.coreutils
+            livedoc-devnet-script
+          ];
+        });
+      };
     });
 
   hydra-pay-exe = pkgs.runCommandNoCC "hydra-pay" {} ''
