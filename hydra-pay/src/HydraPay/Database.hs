48ec8d6c1dd342e510ad3618724b1149799081d9
Sky O <soraskymizu@gmail.com>
2023-04-25 08:56:07 +0000

    Persist Payment Channels

--- hydra-pay/src/HydraPay/Database.hs
+++ hydra-pay/src/HydraPay/Database.hs
@@ -3,11 +3,17 @@
 module HydraPay.Database where
 
 import Control.Lens
+import Control.Monad.IO.Class
+
+import Data.Int
+import Data.Pool
 import Data.Proxy (Proxy(..))
 import Data.Text (Text)
 import Database.Beam
 import Database.Beam.Postgres
+import Database.Beam.Backend.SQL
 import qualified Database.Beam.AutoMigrate as BA
+import Database.PostgreSQL.Simple (Connection, withTransaction)
 import qualified Cardano.Api as Api
 
 data ProxiesT f = ProxyInfo
@@ -31,12 +37,43 @@ instance Beamable (PrimaryKey ProxiesT)
 
 type ProxyInfo = ProxiesT Identity
 
+data PaymentChannelsT f = PaymentChannel
+  { _paymentChannel_id :: C f (SqlSerial Int32)
+  , _paymentChannel_name :: C f Text
+  , _paymentChannel_first :: C f Text
+  , _paymentChannel_second :: C f Text
+  }
+  deriving (Generic)
+
+instance Beamable PaymentChannelsT
+
+instance Table PaymentChannelsT where
+  data PrimaryKey PaymentChannelsT f = PaymentChannelID (C f (SqlSerial Int32))
+    deriving (Generic)
+  primaryKey = PaymentChannelID . _paymentChannel_id
+
+instance Beamable (PrimaryKey PaymentChannelsT)
+
+type PaymentChannel = PaymentChannelsT Identity
+
+makeLenses ''PaymentChannelsT
+
+paymentChannelId :: PaymentChannel -> Int32
+paymentChannelId = unSerial . _paymentChannel_id
+
 data Db f = Db
   { _db_proxies :: f (TableEntity ProxiesT)
+  , _db_paymentChannels :: f (TableEntity PaymentChannelsT)
   }
   deriving (Generic)
 
 instance Database be Db
+
+class HasDbConnectionPool a where
+  dbConnectionPool :: Lens' a (Pool Connection)
+
+instance HasDbConnectionPool (Pool Connection) where
+  dbConnectionPool = id
 
 db :: DatabaseSettings Postgres Db
 db = defaultDbSettings
@@ -53,3 +90,9 @@ doAutomigrate =
 
 makeLenses ''ProxiesT
 makeLenses ''Db
+
+runQueryInTransaction :: (HasDbConnectionPool a, MonadIO m) => a -> (Connection -> IO b) -> m b
+runQueryInTransaction a action = liftIO $ withResource pool $ \conn -> do
+  withTransaction conn (action conn)
+  where
+    pool = a ^. dbConnectionPool
