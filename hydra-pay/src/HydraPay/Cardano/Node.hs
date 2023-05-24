222deea66de41041e64056696e93838fef45e57c
Luigy Leon <luigy@outlook.com>
2023-05-24 07:46:07 -0400

    Delegate ctlc on cardano-node spawned process
    this ensures we allow cardano-node to do its cleanup

--- hydra-pay/src/HydraPay/Cardano/Node.hs
+++ hydra-pay/src/HydraPay/Cardano/Node.hs
@@ -12,6 +12,8 @@ import System.Process
 
 import Control.Lens hiding (parts)
 import Control.Concurrent.STM
+import Control.Exception
+import Control.Monad
 import Control.Monad.IO.Class
 
 import Reflex
@@ -71,24 +73,26 @@ withCardanoNode cfg action = do
   createDirectoryIfMissing True $ cfg ^. nodeConfig_databasePath
   withFile (cfg ^. nodeConfig_databasePath <> "/node.log") AppendMode $ \outHandle -> do
     withFile (cfg ^. nodeConfig_databasePath <> "/node_error.log") AppendMode $ \errHandle -> do
-      withCreateProcess (makeNodeProcess outHandle errHandle cfg) $ \_ _ _ _ -> do
+      withCreateProcess (makeNodeProcess outHandle errHandle cfg) $ \_ _ _ ph -> do
         ready <- newEmptyTMVarIO
         let
           ninfo = NodeInfo (cfg ^. nodeConfig_socketPath) (cfg ^. nodeConfig_magic) ready
           (path, file) = pathAndFile $ ninfo ^. nodeInfo_socketPath
 
         -- Fork the FRP layer so we can still run the action
-        _ <- forkIO $ runHost $ do
+        socketWatchThreadId <- forkIO $ runHost $ do
           changed <- Watch.watchDir FS.defaultConfig path $ socketPred file
           performEvent_ $ ffor changed $ const $ do
             liftIO $ atomically $ putTMVar ready ()
 
-        action ninfo
+        _ <- forkIO $ void $ waitForProcess ph
+        action ninfo `finally` killThread socketWatchThreadId
 
 makeNodeProcess :: Handle -> Handle -> NodeConfig -> CreateProcess
 makeNodeProcess outHandle errHandle (NodeConfig configPath dbPath socketPath topo _) =
   cardanoCp { std_out = UseHandle outHandle
             , std_err = UseHandle errHandle
+            , delegate_ctlc = True
             }
   where
     cardanoCp = proc cardanoNodePath
