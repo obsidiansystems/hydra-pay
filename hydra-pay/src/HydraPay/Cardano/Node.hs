3eb86ede60fdafe570173d3fdf048f2f2538dd5a
Luigy Leon <luigy@outlook.com>
2023-05-24 07:04:55 -0400

    Create db directory if missing
    This fixes running the node on a fresh machine

--- hydra-pay/src/HydraPay/Cardano/Node.hs
+++ hydra-pay/src/HydraPay/Cardano/Node.hs
@@ -5,6 +5,7 @@ module HydraPay.Cardano.Node where
 import Data.Int
 import qualified Data.Text as T
 
+import System.Directory (createDirectoryIfMissing)
 import System.IO
 import System.Which
 import System.Process
@@ -67,6 +68,7 @@ ensureNodeSocket a = liftIO $ do
 
 withCardanoNode :: NodeConfig -> (NodeInfo -> IO a) -> IO a
 withCardanoNode cfg action = do
+  createDirectoryIfMissing True $ cfg ^. nodeConfig_databasePath
   withFile (cfg ^. nodeConfig_databasePath <> "/node.log") AppendMode $ \outHandle -> do
     withFile (cfg ^. nodeConfig_databasePath <> "/node_error.log") AppendMode $ \errHandle -> do
       withCreateProcess (makeNodeProcess outHandle errHandle cfg) $ \_ _ _ _ -> do
