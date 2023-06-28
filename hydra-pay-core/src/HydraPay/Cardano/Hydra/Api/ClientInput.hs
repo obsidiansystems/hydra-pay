24c37ebae287582429d1d22baf0e4960af050afe
Sky O <soraskymizu@gmail.com>
2023-06-28 18:11:56 +0000

    Adjust draft transaction types

--- hydra-pay-core/src/HydraPay/Cardano/Hydra/Api/ClientInput.hs
+++ hydra-pay-core/src/HydraPay/Cardano/Hydra/Api/ClientInput.hs
@@ -4,7 +4,12 @@ module HydraPay.Cardano.Hydra.Api.ClientInput where
 
 import GHC.Generics
 import Data.Aeson
-
+import Data.Aeson.Types
+import Data.Text (Text)
+import Data.Set as Set
+import qualified Data.Map as Map
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as T
 
 data ClientInput
   = Init
@@ -20,10 +25,20 @@ data ClientInput
 instance FromJSON ClientInput
 instance ToJSON ClientInput
 
-data DraftCommitTxRequest = DraftCommitTxRequest
-  { utxos :: Value
-  }
+data DraftCommitTxRequest
+  = DraftCommitTxRequest { utxos :: Value }
+  | DraftCommitRequestPlaceholder
+  -- ^ Ensure we encode a "tag"
   deriving (Generic)
 
 instance FromJSON DraftCommitTxRequest
 instance ToJSON DraftCommitTxRequest
+
+data DraftCommitTxResponse
+  = DraftCommitTxResponse { commitTx :: Text }
+  | DraftCommitResponsePlaceholder
+  -- ^ Ensure we encode a "tag"
+  deriving (Generic)
+
+instance FromJSON DraftCommitTxResponse
+instance ToJSON DraftCommitTxResponse
